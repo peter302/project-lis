958e5db49a9632d943c41128f5b90255dc5cf222
peter kuria <you@example.com>
2020-02-17 01:33:32 -0500

    added post serializers

--- p_profile/serializer.py
+++ p_profile/serializer.py
@@ -5,3 +5,9 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['title','image', 'description', 'user', 'date_posted', 'link', 'country']
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['user', 'profile_picture', 'bio', 'contact']
