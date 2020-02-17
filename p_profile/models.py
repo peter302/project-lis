dd62028e6aa4d97a46ae53971d85654e6e44f471
peter kuria <you@example.com>
2020-02-17 00:17:54 -0500

    created profile model

--- p_profile/models.py
+++ p_profile/models.py
@@ -5,3 +5,8 @@ from django.db.models import Q
 
 
 # Create your models here.
+class Profile(models.Model):
+    user= models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture =models.ImageField(upload_to= 'profiles/', blank=True, default="profiles/a.jpg")
+    bio = models.CharField(max_length=100, default='Welcome to you bio')
+    contact = models.CharField(max_length=80)
