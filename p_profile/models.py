022bf7a8649d08f78709f1f71d44dac38156faaf
peter kuria <you@example.com>
2020-02-17 00:21:50 -0500

    added get profiles and search user to profile models

--- p_profile/models.py
+++ p_profile/models.py
@@ -19,3 +19,16 @@ class Profile(models.Model):
 
     def delete_profile(self):
         self.delete()
+
+        @classmethod
+    def update_bio(cls,id, bio):
+        update_profile = cls.objects.filter(id = id).update(bio = bio)
+        return update_profile
+
+    @classmethod
+    def get_all_profiles(cls):
+        profile = Profile.objects.all()
+        return profile
+    @classmethod
+    def search_user(cls,user):
+        return cls.objects.filter(user__username__icontains=user).all()    
