c60d33bcc97e87bbe17c4f319aebe6d7d3e89048
peter kuria <you@example.com>
2020-02-17 00:34:14 -0500

    made various imports to views.py

--- p_profile/views.py
+++ p_profile/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect,get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import Profile, Post
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import PostSerializer, ProfileSerializer
+import datetime as dt
+from . forms import ProfileForm, PostForm
