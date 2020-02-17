f6f09be7113876fa6ad62ed29bc1b12a1eca8308
peter kuria <you@example.com>
2020-02-17 01:32:53 -0500

    added profile serializers

--- p_profile/serializer.py
+++ p_profile/serializer.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import Profile, Post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['title','image', 'description', 'user', 'date_posted', 'link', 'country']
