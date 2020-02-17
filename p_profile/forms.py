917a950911f88e7ddf59f3b04552ea9115d535f0
peter kuria <you@example.com>
2020-02-17 02:01:21 -0500

    created postform class

--- p_profile/forms.py
+++ p_profile/forms.py
@@ -5,3 +5,8 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude=["user"]
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = []        
