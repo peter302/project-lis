76867ac37d6d52666ba714dd63dce7937ed72734
peter kuria <you@example.com>
2020-02-19 03:23:23 -0500

    added

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-MODE=config("MODE", default="dev")
+MODE=config('MODE', default="dev")
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # development
